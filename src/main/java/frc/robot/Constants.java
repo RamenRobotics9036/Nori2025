efeda3f9b8186ba49a0daeb1c0fe2b87079ae84d
Ramen <ramen90bot1@gmail.com>
2024-10-21 17:36:35 -0700

    Merge code from 2024 and YAGSL example

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,13 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.util.PIDConstants;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,8 +19,44 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+public final class Constants
+{
+  /**
+   * Constants for the joystick.
+   */
+  public static class OperatorConstants
+  {
+    public static final int kDriverPort = 0;
+    public static final int kArmPort = 1;
+    public static final double kExpo = 4; //do not change this value
+    public static final double kExpoRatio = 0.6; // change this 0..1 to add more exponential, 0 = no expo (linear)
+    public static final double kDeadband = 0.07;
+  }
+  /**
+   * Constants for the swerve system.
+   */
+  public static class SwerveConstants
+  {
+    public static final String  kJsonDirectory = "swerve";
+    public static final double kMaxSpeedMetersPerSecond = 5.06;
+    public static final double kRobotMass = Units.lbsToKilograms(120); // TODO: update
+    public static final Matter kChassisMatter = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), kRobotMass); // TODO: update
+    public static final double kLoopTime = 0.13; //s, 20ms + 110ms sprk max velocity lag
+  }
+  /**
+   * Constants for telemetry.
+   */
+  public static class TelemetryConstants
+  {
+    public static final TelemetryVerbosity kSwerveVerbosity = TelemetryVerbosity.HIGH;
+  }
+  /**
+   * Constants for autonomous mode.
+   */
+  public static final class AutoConstants
+  {
+    public static final PIDConstants kTranslationpid = new PIDConstants(5, 0, 0); // try 0.7,0,0 from YAGSL-Example
+    public static final PIDConstants kAnglePID = new PIDConstants(5, 0, 0.1); // try 0.4,0,0.01 from YAGSL-Example
+    public static final double kMaxSpeedMetersPerSecond = 5.06; // try 4.5 from YAGSL-Example
   }
 }
---
+++ src/main/java/frc/robot/util/AppliedController.java
@@ -0,0 +1,73 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.OperatorConstants;
+
+/**
+ * AppliedController.
+ */
+public class AppliedController extends XboxController {
+
+    private double m_controllerExponent; //expo factor for the analog axises
+
+    /**
+     * Constructor.
+     */
+    public AppliedController(int port) {
+        super(port);
+        this.m_controllerExponent = Math.pow(OperatorConstants.kExpo, OperatorConstants.kExpoRatio);
+    }
+
+    public double expo(double input, double exponent) {
+        if (input == 0.0) return 0.0;
+        double expo = Math.pow(Math.abs(input), exponent);
+        return input < 0 ? -expo : expo;
+    }
+
+    /**
+     * Overrides.
+     */
+
+    @Override
+    public double getLeftY() {
+        return expo(
+            MathUtil.applyDeadband(super.getLeftY(), OperatorConstants.kDeadband),
+            m_controllerExponent);
+    }
+
+    @Override
+    public double getRightY() {
+        return expo(
+            MathUtil.applyDeadband(super.getRightY(), OperatorConstants.kDeadband),
+            m_controllerExponent);
+    }
+
+    @Override
+    public double getLeftX() {
+        return expo(
+            MathUtil.applyDeadband(super.getLeftX(), OperatorConstants.kDeadband),
+            m_controllerExponent);
+    }
+
+    @Override
+    public double getRightX() {
+        return expo(
+            MathUtil.applyDeadband(super.getRightX(), OperatorConstants.kDeadband),
+            m_controllerExponent);
+    }
+
+    @Override
+    public double getLeftTriggerAxis() {
+        return expo(
+            MathUtil.applyDeadband(super.getLeftTriggerAxis(), OperatorConstants.kDeadband),
+            m_controllerExponent);
+    }
+
+    @Override
+    public double getRightTriggerAxis() {
+        return expo(
+            MathUtil.applyDeadband(super.getRightTriggerAxis(), OperatorConstants.kDeadband),
+            m_controllerExponent);
+    }
+}
