9165e5713d436350925dc07e675ea4e8b97244b9
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2025-01-18 13:27:29 -0800

    Update for new limelight position.

--- src/main/java/frc/robot/commands/AlignRobot.java
+++ src/main/java/frc/robot/commands/AlignRobot.java
@@ -17,16 +17,18 @@ public class AlignRobot extends Command {
 
     private PIDController m_drivePIDController = new PIDController(2, 0, 0);
     private PIDController m_strafePIDController = new PIDController(2, 0, 0);
-
+    private PIDController m_rotatePIDController = new PIDController(1, 0, 0);
 
     public AlignRobot(SwerveSubsystem swerveDrive) {
         m_swerveDrive = swerveDrive;
 
         m_drivePIDController.setTolerance(VisionConstants.allowedAngleUncertaintyMeters);
         m_strafePIDController.setTolerance(VisionConstants.allowedAngleUncertaintyMeters);
+        m_rotatePIDController.setTolerance(VisionConstants.allowedAngleUncertaintyDegrees);
 
         m_drivePIDController.setSetpoint(AlignRobotConstants.transformDrive);
         m_strafePIDController.setSetpoint(AlignRobotConstants.transformStrafe);
+        m_rotatePIDController.setSetpoint(AlignRobotConstants.transformRot);
 
         addRequirements(m_swerveDrive);
     }
@@ -42,10 +44,13 @@ public class AlignRobot extends Command {
 
         double drive = m_drivePIDController.calculate(targetPose.getY());
         double strafe = m_strafePIDController.calculate(targetPose.getX());
+        double rotate = -m_rotatePIDController.calculate(targetPose.getRotation().getDegrees());
 
         drive = MathUtil.clamp(drive, -AlignRobotConstants.maxSpeed, AlignRobotConstants.maxSpeed);
         strafe = MathUtil.clamp(strafe, -AlignRobotConstants.maxSpeed, AlignRobotConstants.maxSpeed);
+        rotate = MathUtil.clamp(rotate, -AlignRobotConstants.maxSpeed, AlignRobotConstants.maxSpeed);
 
+        //Zeroing out rotation. not working yet.
         m_swerveDrive.drive(new Translation2d(drive, strafe), 0, false);
     }
 
