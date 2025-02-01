3b2ffafd589c23f0f676817747c954934a643277
Ido <idoben2@hotmail.com>
2025-02-01 15:23:27 -0800

    Empty command for testing turn of one wheel

--- src/main/java/frc/robot/commands/TestTurnWheel.java
+++ src/main/java/frc/robot/commands/TestTurnWheel.java
@@ -4,10 +4,14 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.TestSwerveConstants;
 import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveDrive;
+import swervelib.SwerveModule;
 
 public class TestTurnWheel extends Command {
     private SwerveSubsystem m_swerveDrive;
     private Timer m_timer = new Timer();
+    private static final double TURN_TIME_SECONDS = 2.0;
+    private SwerveModule m_module = null;
 
     // Constructor
     public TestTurnWheel(SwerveSubsystem swerveDrive) {
@@ -19,19 +23,42 @@ public class TestTurnWheel extends Command {
     @Override
     public void initialize() {
         m_timer.restart();
+
+        SwerveModule[] moduleList = m_swerveDrive.getSwerveDrive().getModules();
+        m_module = getSingleModuleByName(moduleList, "frontleft");
     }
 
     @Override
     public void execute() {
+        if (m_module == null) {
+            return;
+        }
+
+        double fraction = Math.min(m_timer.get() / TURN_TIME_SECONDS, 1.0);
+        double angleDegrees = fraction * 90.0;
+    }
+
+    private SwerveModule getSingleModuleByName(SwerveModule[] moduleList, String name) {
+        for (SwerveModule module : moduleList) {
+            if (module.configuration.name.equals(name)) {
+                return module;
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean isFinished() {
+        if (m_module == null) {
+            // End command if there isnt a module named appropriately
+            return true;
+        }
+
         if (m_timer.get() > TestSwerveConstants.maxTimeSeconds) {
             return true;
         }
 
-        return false;
+        return m_timer.get() >= TURN_TIME_SECONDS;
     }
 
     @Override
