a852e544993c1b38477c09889499f9f4e58b2b85
Ramen <ramen90bot1@gmail.com>
2025-02-01 11:53:07 -0800

    added getters and setters to intake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -61,7 +61,6 @@ public final class Constants
   public static final class IntakeConstants{
     public static final int pullMotorID = 5;
     public static final int loadMotorID = 6;
-
-  
+    public static final double maxOutputPercentage = 0.4;
   }
 }
--- src/main/java/frc/robot/subsystems/IntakeSystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSystem.java
@@ -6,17 +6,49 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSystem extends SubsystemBase{
+    //the front motor, for pulling the coarl in and scoring on L1
     private SparkMax m_pullMotor = new SparkMax(IntakeConstants.pullMotorID, MotorType.kBrushless);
+    //the back motor, for loading onto the robot
     private SparkMax m_loadMotor = new SparkMax(IntakeConstants.loadMotorID, MotorType.kBrushless);
     private SparkMaxConfig config = new SparkMaxConfig();
 
+    private double maxOutput = IntakeConstants.maxOutputPercentage;
+
+    //sets the idle mode of both motors to kBrake
     public IntakeSystem(){
         config.idleMode(SparkBaseConfig.IdleMode.kBrake);
         m_pullMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        m_pullMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+        m_loadMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    }
+
+    //sets the speed of m_pullMotor. Cannot exceed maxOutputPercentage
+    public void setPullMotorSpeed(double speed){
+        speed = MathUtil.clamp(speed, -maxOutput, maxOutput);
+        m_pullMotor.set(speed);
+    }
+    //sets the speed of m_loadMotor. Cannot exceed maxOutputPercentage
+    public void setLoadMotorSpeed(double speed){
+        speed = MathUtil.clamp(speed, -maxOutput, maxOutput);
+        m_loadMotor.set(speed);
+    }
+
+    //gets the speed of m_pullMotor
+    public double getPullMotorSpeed(){
+        return m_pullMotor.get();
+    }
+    //gets the speed of m_loadMotor
+    public double getLoadMotorSpeed(){
+        return m_loadMotor.get();
+    }
+
+    //stops everything
+    public void stopSystem(){
+        m_pullMotor.stopMotor();
+        m_loadMotor.stopMotor();
     }
 }
