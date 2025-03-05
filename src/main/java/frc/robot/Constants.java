bce57342d7f31222573dc46c88a804f96960c321
Ramen <ramen90bot1@gmail.com>
2025-03-05 14:59:52 -0800

    Fix elevator inversion and arm offset.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -188,7 +188,7 @@ public final class Constants
     public static final double L1ArmAngle = 1.1;
     public static final double kMinArmRotation = 0.8;
     
-    public static final double kAbsoluteEncoderOffset = 2.0;
+    public static final double kAbsoluteEncoderOffset = 2.0 - 0.2;
     public static final int kcurrentLimit = 20;
     public static final double tolerance = 0.1;
     public static final double setArmMaxTime = 10;
