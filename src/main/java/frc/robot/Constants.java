8eb3d07320dcc8b82f7ba66e61f1c2ef2312f897
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2025-03-10 18:02:48 -0700

    Tweak xbox controls

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,9 +40,9 @@ public final class Constants
   {
     public static final int kDriverPort = 0;
     public static final int kArmPort = 1;
-    public static final double kExpo = 4; //do not change this value
+    public static final double kExpo = 4; //do not change this value, ever!
     public static final double kExpoRatio = 0.5; // change this 0..1 to add more exponential, 0 = no expo (linear)
-    public static final double kDeadband = 0.035;
+    public static final double kDeadband = 0.01;
     public static final Supplier<Alliance> kAlliance = () -> (DriverStation.getAlliance().isPresent()) ? DriverStation.getAlliance().get() : Alliance.Red;
     public static final boolean kCompetitionMode = false; // Set to true the day of competition to turn off extras (max performance)
   }
