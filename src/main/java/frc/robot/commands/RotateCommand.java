45046255b600a66a0110d2705ee3806087431456
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2025-03-25 15:45:15 -0700

    Added conditionals and everything except turning.

--- src/main/java/frc/robot/commands/RotateCommand.java
+++ src/main/java/frc/robot/commands/RotateCommand.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class RotateCommand extends Command{
@@ -21,5 +23,31 @@ public class RotateCommand extends Command{
         m_desiredAngle = 90;
 
         addRequirements(m_swerve);
+    }
+
+    @Override
+    public void initialize() {
+        m_desiredAngle += m_swerve.getPose().getRotation().getDegrees();
+
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+    }
+
+    @Override
+    public boolean isFinished() {
+        //timeout
+        if (m_Timer.get() >= AutoConstants.kRotTimeout) { //TODO: filler value
+            return true;
+        }
+        return MathUtil.applyDeadband(m_swerve.getPose().getRotation().getDegrees() - m_desiredAngle, AutoConstants.kRotMarginOfError) == 0; //TODO: filler value
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_swerve.stopSystem();
     }
 }
