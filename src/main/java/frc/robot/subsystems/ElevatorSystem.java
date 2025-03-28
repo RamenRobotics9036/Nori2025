9a4e5b789323c1446bb48aea84eeaed008e294ec
Ido <idoben2@hotmail.com>
2025-03-28 15:13:28 -0700

    Part2 of elevator simulation

--- src/main/java/frc/robot/subsystems/ElevatorSystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSystem.java
@@ -8,6 +8,7 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -22,6 +23,9 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.logging.TriviaLogger;
+import frc.robot.sim.SimConstants;
+import frc.robot.sim.SimConstants.ElevatorSimConstants;
+import frc.robot.sim.elevatorsimulation.ElevatorSimulation;
 
 public class ElevatorSystem extends SubsystemBase{
     //Motors are on opposite sides of a shaft
@@ -35,6 +39,7 @@ public class ElevatorSystem extends SubsystemBase{
     private double m_desiredPosition;
     private double m_encoderPosition;
     private boolean m_limitReached = false;
+    private ElevatorSimulation m_elevatorSim = null;
 
     private DigitalInput m_limitSwitch= new DigitalInput(ElevatorConstants.kDIOIndex);
     /* Sensor will reset a relative encoder when the elevator lowers fully
@@ -79,6 +84,10 @@ public class ElevatorSystem extends SubsystemBase{
             SparkBase.PersistMode.kPersistParameters);
 
         m_encoderPosition = m_encoder.getPosition();
+
+        if (RobotBase.isSimulation()) {
+            m_elevatorSim = new ElevatorSimulation();
+        }
 
         initShuffleboard();
     }
@@ -134,6 +143,20 @@ public class ElevatorSystem extends SubsystemBase{
                     // initializeMotorConfig(false);
                 }
         }
+
+        if (m_elevatorSim != null) {
+            // Run the simulation model.
+            m_elevatorSim.reachGoal(ElevatorSimConstants.kSetpointMeters);
+        }
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        if (m_elevatorSim != null) {
+            // Update the simulation model.
+            m_elevatorSim.simulationPeriodic();
+            m_elevatorSim.updateTelemetry();
+        }
     }
 
     public void setPosition(double position){
@@ -172,5 +195,10 @@ public class ElevatorSystem extends SubsystemBase{
 
     public void stopSystem(){
         m_leaderMotor.stopMotor();
+
+        if (m_elevatorSim != null) {
+            // This just makes sure that our simulation code knows that the motor's off.
+            m_elevatorSim.stop();
+        }
     }
 }
