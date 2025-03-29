5c955585f927a44523ea6e57876966c43c3983a3
Ramen <ramen90bot1@gmail.com>
2025-03-29 12:00:56 -0700

    Made get methods use cached values

--- src/main/java/frc/robot/subsystems/IntakeArmSystem.java
+++ src/main/java/frc/robot/subsystems/IntakeArmSystem.java
@@ -98,8 +98,14 @@ public class IntakeArmSystem extends SubsystemBase{
         }
 
         //Caching values
-        m_armAngleAbsolute = getArmAngle();
-        m_armAngleRelative = getArmAngleRelative();
+        if (RobotBase.isSimulation()) { 
+            m_armAngleAbsolute = m_armEncoder.get(); 
+        } else{
+            m_armAngleAbsolute = Math.max(0, (m_armEncoder.get() * 2 * Math.PI) + ArmConstants.kAbsoluteEncoderOffset) % (Math.PI * 2);
+        }
+
+        m_armAngleRelative = m_armRelativeEncoder.getPosition();
+
 
         if (!m_armEncoder.isConnected()) { 
             m_armRelativeEncoder.setPosition(0.0); 
@@ -138,8 +144,14 @@ public class IntakeArmSystem extends SubsystemBase{
     public void periodic() { 
         
         //Caching values
-        m_armAngleAbsolute = getArmAngle();
-        m_armAngleRelative = getArmAngleRelative();
+        if (RobotBase.isSimulation()) { 
+            m_armAngleAbsolute = m_armEncoder.get(); 
+        } else{
+            m_armAngleAbsolute = Math.max(0, (m_armEncoder.get() * 2 * Math.PI) + ArmConstants.kAbsoluteEncoderOffset) % (Math.PI * 2);
+        }
+
+        m_armAngleRelative = m_armRelativeEncoder.getPosition();
+        m_armAngleRelative = m_armRelativeEncoder.getPosition();
 
 
         // loop += 1; 
@@ -200,7 +212,7 @@ public class IntakeArmSystem extends SubsystemBase{
 
     public void setReference(double position) { 
         m_desiredAngle = position; 
-        m_armPIDController.setReference(position, ControlType.kPosition); 
+        // m_armPIDController.setReference(position, ControlType.kPosition); 
     } 
  
     //sets the speed of m_armMotor. Cannot exceed maxOutputPercentage 
@@ -212,17 +224,11 @@ public class IntakeArmSystem extends SubsystemBase{
  
     // get encoder value 
     private double getArmAngle() {
-        if (RobotBase.isSimulation()) { 
-            return m_armEncoder.get(); 
-        }
-
-        /// $TODO This seems like a bug.  Conversion factor was already configured on the absolute encoder, 
-        // so units are already in radians.  Needs investigation.
-        return Math.max(0, (m_armEncoder.get() * 2 * Math.PI) + ArmConstants.kAbsoluteEncoderOffset) % (Math.PI * 2); 
+        return m_armAngleAbsolute;
     } 
  
     public double getArmAngleRelative() { 
-        return m_armRelativeEncoder.getPosition(); 
+        return m_armAngleRelative;
     } 
  
     // public double getAbsoluteArmAngle() { 
