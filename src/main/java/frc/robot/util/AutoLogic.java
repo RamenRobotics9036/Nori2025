621a6a6be076c51122671d73948eb152ad75e07e
Ramen bot Sam1 <ramen9036bot1@gmail.com>
2025-11-04 17:21:38 -0800

    Adds a new auto routine

--- src/main/java/frc/robot/util/AutoLogic.java
+++ src/main/java/frc/robot/util/AutoLogic.java
@@ -131,7 +131,7 @@ public class AutoLogic {
   public static void addAutoOptions() {
     //autoPicker.setDefaultOption("Right auto", "Birdi Auto Right");
     //autoPicker.setDefaultOption("Left Auto", "Birdi Auto left");
-    autoPicker.setDefaultOption("Center Auto", "Birdi Auto Center");
+    autoPicker.setDefaultOption("Ronen Auto", "Ronen Auto");
     //autoPicker.setDefaultOption("test auto freedom project", "TTTTest");
     
     //This was used for the manual code of the Auto
